d34a5967454fb36aef5a488ab047e30aa3e8f5c5
Xing Ruan <xing.ruan@fedex.com>
2021-12-27 23:40:42 -0600

    First commit

---
+++ src/main/java/com/clientmanager/springboot/SpringbootAwsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.clientmanager.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clientmanager/springboot/SpringbootAwsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientmanager.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
